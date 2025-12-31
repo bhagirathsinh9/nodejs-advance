5cfa16370093bd4c9196844c46a060069763dfa2
bhagirathsinh9 <bhagirathn.encircle@gmail.com>
2025-12-31 19:09:16 +0530

    docs : prettier documentation added

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,16 @@
-const express = require("express");
-const cron = require("node-cron");
-const fs = require("fs");
+const express = require('express')
+const cron = require('node-cron')
+const fs = require('fs')
 
-const app = express();
+const app = express()
 
 //Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Creating a cron job which runs on every 10:00 AM, except Saturday and Sunday
 // cron.schedule('0 10 * * 1-5', function() {
 //     console.log("Running a task every day at 10:00 AM, except Saturday and Sunday");
 // });
 
-
-
-module.exports = { app };
+module.exports = { app }
--- Backend/index.js
+++ Backend/index.js
@@ -1,43 +1,43 @@
 // index.js
-const http = require("http");
-const { app } = require("./app");
-const { Server } = require("socket.io");
+const http = require('http')
+const { app } = require('./app')
+const { Server } = require('socket.io')
 
-const PORT = 5000;
-const server = http.createServer(app);
+const PORT = 5000
+const server = http.createServer(app)
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
-});
+  cors: { origin: 'http://localhost:5173', methods: ['GET', 'POST'] },
+})
 
 // In-memory blogs and comments
-const blogs = []; 
-const comments = {}; 
+const blogs = []
+const comments = {}
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id)
 
-  socket.emit("load_blogs", blogs);
+  socket.emit('load_blogs', blogs)
 
-  socket.on("new_blog", (blog) => {
-    blogs.push(blog);
-    io.emit("receive_blog", blog);
-  });
+  socket.on('new_blog', (blog) => {
+    blogs.push(blog)
+    io.emit('receive_blog', blog)
+  })
 
   // Join blog room for comments
-  socket.on("join_blog", (blogId) => {
-    socket.join(blogId);
-    socket.emit("load_comments", comments[blogId] || []);
-  });
+  socket.on('join_blog', (blogId) => {
+    socket.join(blogId)
+    socket.emit('load_comments', comments[blogId] || [])
+  })
 
   // New comment
-  socket.on("new_comment", ({ blogId, comment }) => {
-    if (!comments[blogId]) comments[blogId] = [];
-    comments[blogId].push(comment);
-    io.to(blogId).emit("receive_comment", comment);
-  });
+  socket.on('new_comment', ({ blogId, comment }) => {
+    if (!comments[blogId]) comments[blogId] = []
+    comments[blogId].push(comment)
+    io.to(blogId).emit('receive_comment', comment)
+  })
 
-  socket.on("disconnect", () => console.log("User disconnected:", socket.id));
-});
+  socket.on('disconnect', () => console.log('User disconnected:', socket.id))
+})
 
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on ${PORT}`))
